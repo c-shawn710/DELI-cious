63d9fafd6c46e8e6b9103b67d973835d631d3fb7
Shawn_Chung <c_shawn710@yahoo.com>
2024-11-08 15:09:08 -0800

    sandwich class

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -59,14 +59,23 @@ public class Sandwich implements Orderable {
         this.toasted = toasted;
     }
 
+    //custom methods
+    public void addTopping(String topping) {
+        toppings.add(topping);
+    }
+
+    public void addSauce(String sauce) {
+        sauces.add(sauce);
+    }
+
     @Override
     public double getPrice() {
         double price = 0;
-        if (size.equals("4")) {
+        if (size.equalsIgnoreCase("small")) {
             price = 5.50;
-        } else if (size.equals("8")) {
+        } else if (size.equalsIgnoreCase("medium")) {
             price = 7.00;
-        } else if (size.equals("12")) {
+        } else if (size.equalsIgnoreCase("large")) {
             price = 8.50;
         }
         return price;
@@ -74,11 +83,54 @@ public class Sandwich implements Orderable {
 
     @Override
     public String getDescription() {
-        return "";
+        return "Sandwich - " + size + ", " + breadType + (toasted ? ", toasted" : "") + "\nToppings: " + toppings + "\nSauces: " + sauces;
     }
 
     @Override
     public void customizeItem(Scanner scanner) {
+        System.out.println("Choose bread type: White, Wheat, Rye, Wrap");
+        setBreadType(scanner.nextLine());
 
+        System.out.println("Choose size: Small(4\") - $5.50, Medium(8\") - $7.00, Large(12\") - $8.50");
+        setSize(scanner.nextLine());
+
+        String topping;
+        do {
+            System.out.println("Add toppings: Type 'Done' to finish\n" +
+                    "- Lettuce\n" +
+                    "- Peppers\n" +
+                    "- Onions\n" +
+                    "- Tomatoes\n" +
+                    "- Jalepenos\n" +
+                    "- Cucumbers\n" +
+                    "- Pickles\n" +
+                    "- Guacamole\n" +
+                    "- Mushrooms");
+
+            topping = scanner.nextLine();
+            if (!topping.equalsIgnoreCase("done")) {
+                addTopping(topping);
+            }
+        } while (!topping.equalsIgnoreCase("done"));
+
+        System.out.println("Add sauces: Type 'Done' to finish\n" +
+                     "- Mayo\n" +
+                     "- Mustard\n" +
+                     "- Ketchup\n" +
+                     "- Ranch\n" +
+                     "- Thousand Sslands\n" +
+                     "- Vinaigrette");
+
+        String sauce;
+        do {
+            sauce = scanner.nextLine();
+            if (!sauce.equalsIgnoreCase("done")) {
+                addSauce(sauce);
+            }
+        } while (!sauce.equalsIgnoreCase("done"));
+
+        System.out.println("Would you like the sandwich toasted? (yes/no):");
+        setToasted(scanner.nextLine().equalsIgnoreCase("yes"));
     }
 }
+
