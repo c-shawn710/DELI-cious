cb85fe324b86745ea9a69c6f9c4a3652bae87377
Shawn_Chung <c_shawn710@yahoo.com>
2024-11-08 17:01:25 -0800

    chips class

---
+++ src/main/java/com/pluralsight/Chips.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Chips implements Orderable {
+    private String type;
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public void customizeItem(Scanner scanner) {
+        //List of available chips
+        String[] availableChips = {"Lays", "Ruffles", "Doritos", "Kettle", "Cheetos"};
+        boolean validChips = false;
+
+        while (!validChips) {
+            System.out.println("Choose type of chips: Lays, Ruffles, Doritos, Kettle, Cheetos");
+            type = scanner.nextLine();
+
+            //Check if user input is valid chips
+            for (String availableChip : availableChips) {
+                if (type.equalsIgnoreCase(availableChip)) {
+                    validChips = true;
+                    type = availableChip; //Match capitalization with our list
+                    break;
+                }
+            }
+            if(!validChips) {
+                System.out.println("Please choose from the available options: Lays, Ruffles, Doritos, Kettle, Cheetos");
+            }
+        }
+    }
+}
